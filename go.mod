d22243bbb9b1209f32c4451b6485e8397b1c6030
Aniruddha Sinha <asinha0493@gmail.com>
2026-07-01 14:55:52 +0530

    added interfaces and client structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aniruddha-sinha/go-web-client
+
+go 1.26.4
---
+++ web-client.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var (
+	ErrReadResponse = errors.New("errors reading response")
+)
+
+type ClientAPI interface {
+	Do(ctx context.Context, url, method string, headers map[string]string, payload any) (*http.Response, error)
+}
+
+type Client struct {
+	httpClient *http.Client
+}
+
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	return &Client{
+		httpClient: httpClient,
+	}
+}
+
+func (c *Client) requestBuilder(ctx context.Context, url, method string, headers map[string]string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	//set the default headers
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+
+	//custom headers
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	return req, nil
+}
+
+func statusCodeAnalyzer(res *http.Response) (*http.Response, error) {
+	if res.StatusCode >= 300 {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, ErrReadResponse
+		}
+		_ = res.Body.Close()
+		return nil, fmt.Errorf("Client API error (status : %d): %s", res.StatusCode, string(bodyBytes))
+	}
+
+	return res, nil
+}
+
+func (c *Client) Do(ctx context.Context, method, url string, headers map[string]string, body io.Reader) (*http.Response, error) {
+	req, err := c.requestBuilder(ctx, method, url, headers, body)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return statusCodeAnalyzer(res)
+}
